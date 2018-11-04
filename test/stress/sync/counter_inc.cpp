7a55e438445d4a2c5b9ffac0b47379349606ff89
Or Ostrovsky <or.ostrovsky@gmail.com>
2018-11-04 11:13:23 +0200

    Added test to check that htm + atomics work together

--- test/stress/sync/counter_inc.cpp
+++ test/stress/sync/counter_inc.cpp
@@ -25,8 +25,8 @@ namespace {
             size_t m_nSuccess = 0;
 
           public:
-            Worker(cds_test::thread_pool &pool, counter_type &s)
-                : base_class(pool), m_counter(s) {}
+            Worker(cds_test::thread_pool &pool, counter_type &s, int type = 0)
+                : base_class(pool, type), m_counter(s) {}
 
             Worker(Worker &src) : base_class(src), m_counter(src.m_counter) {}
 
@@ -71,9 +71,8 @@ namespace {
 
             std::chrono::milliseconds duration = pool.run();
 
-            propout() << std::make_pair("duration", duration);
-
-            std::cout << "[ MESSAGE  ] Total: " << nTotal << std::endl;
+            propout() << std::make_pair("duration", duration)
+              << std::make_pair("nTotal", nTotal.load(std::memory_order_relaxed));
 
             // analyze result
 
@@ -92,13 +91,15 @@ namespace {
     size_t counter_inc::s_nThreadCount = 4;
     size_t counter_inc::s_nIncrementCount = 100000;
 
-    static void spin(size_t n) { for(volatile size_t i = 0; i < n; ++i) {} }
+    static void spin(size_t n, std::true_type) { for(volatile size_t i = 0; i < n; ++i) {} }
+    static void spin(size_t, std::false_type) { }
 
+    template <class Delay>
     struct HTMPolicy {
       typedef atomics::atomic_size_t counter_type;
       static bool _(counter_type& counter) {
         auto tmp = counter.load(atomics::memory_order_acquire);
-        spin(1000);
+        spin(1000, Delay{});
         auto res = cds::sync::htm([&] {
           if (tmp != counter.load(atomics::memory_order_relaxed)) cds::sync::abort<0xff>();
           counter.store(tmp + 1, atomics::memory_order_relaxed);
@@ -107,17 +108,74 @@ namespace {
       }
     };
 
+    template <class Delay>
     struct CASPolicy {
       typedef atomics::atomic_size_t counter_type;
       static bool _(counter_type& counter) {
         auto old = counter.load(atomics::memory_order_acquire);
-        spin(1000);
+        spin(1000, Delay{});
         return counter.compare_exchange_weak(old, old + 1, atomics::memory_order_release, atomics::memory_order_relaxed);
       }
     };
 
 #ifdef CDS_HTM_SUPPORT
-    TEST_F(counter_inc, htm) { test<HTMPolicy>(); }
-    TEST_F(counter_inc, cas) { test<CASPolicy>(); }
+    TEST_F(counter_inc, htm) { test<HTMPolicy<std::true_type>>(); }
+    TEST_F(counter_inc, cas) { test<CASPolicy<std::true_type>>(); }
 #endif // CDS_HTM_SUPPORT
+
+    class dual_counter_inc : public counter_inc {
+      protected:
+        enum {
+          cas_worker,
+          htm_worker
+        };
+        static void SetUpTestCase() {
+            counter_inc::SetUpTestCase();
+            s_nThreadCount = (s_nThreadCount / 2) * 2;
+        }
+
+        void test() {
+            cds_test::thread_pool &pool = get_pool();
+
+            typename atomics::atomic_size_t nTotal{0};
+
+            pool.add(new Worker<CASPolicy<std::false_type>>(pool, nTotal), s_nThreadCount / 2);
+            pool.add(new Worker<HTMPolicy<std::false_type>>(pool, nTotal), s_nThreadCount / 2);
+
+            propout() << std::make_pair("work_thread", s_nThreadCount)
+                      << std::make_pair("increment_count", s_nIncrementCount);
+
+            std::chrono::milliseconds duration = pool.run();
+
+            propout() << std::make_pair("duration", duration)
+              << std::make_pair("nTotal", nTotal.load(atomics::memory_order_relaxed));
+
+            // analyze result
+
+            size_t nSuccess = 0;
+            for (size_t threadNo = 0; threadNo < pool.size(); ++threadNo) {
+              auto& thr = pool.get(threadNo);
+              if (thr.type() == cas_worker) {
+                nSuccess +=
+                    static_cast<Worker<CASPolicy<std::false_type>> &>(pool.get(threadNo)).m_nSuccess;
+              } else {
+                nSuccess +=
+                    static_cast<Worker<HTMPolicy<std::false_type>> &>(pool.get(threadNo)).m_nSuccess;
+              }
+            }
+
+            EXPECT_EQ(nSuccess, nTotal);
+            EXPECT_NE(0, nTotal);
+
+            propout() << std::make_pair("total", nSuccess);
+        }
+    };
+
+
+#ifdef CDS_HTM_SUPPORT
+    TEST_F(dual_counter_inc, dual) { test(); }
+#endif // CDS_HTM_SUPPORT
+
+
+
 } // namespace
