a6ad000964ff017eae64c74600c5bd15b3961dad
Or Ostrovsky <or.ostrovsky@gmail.com>
2019-03-28 22:10:30 +0200

    Try to do a better simple queue

--- cds/container/bags.h
+++ cds/container/bags.h
@@ -23,42 +23,71 @@ namespace cds { namespace container {
         class SimpleBag
         {
         private:
-            using value_type = PaddedValue<T>;
-            atomics::atomic_int m_counter;
-            char pad2_[cds::c_nCacheLineSize - sizeof(m_counter)];
+            struct flagged_value {
+              T value;
+              atomics::atomic_bool flag{false};
+            };
+            using value_type = PaddedValue<flagged_value>;
+            atomics::atomic_int m_pushes{0};
+            char pad1_[cds::c_nCacheLineSize - sizeof(m_pushes)];
+            atomics::atomic_int m_pops{0};
+            char pad2_[cds::c_nCacheLineSize - sizeof(m_pops)];
+            static constexpr int NO_SIZE = -1;
+            atomics::atomic_int m_real_size{NO_SIZE};
+            char pad3_[cds::c_nCacheLineSize - sizeof(m_real_size)];
             static constexpr size_t MAX_THREADS=40;
             std::array<value_type, MAX_THREADS> m_bag;
             size_t m_size;
 
         public:
-            SimpleBag(size_t ids) : m_counter(0), m_size(ids) {
+            static constexpr const size_t c_nHazardPtrCount = 0; ///< Count of hazard pointer required for the algorithm
+            SimpleBag(size_t ids) : m_size(ids) {
               assert(m_size <= MAX_THREADS);
             }
             bool insert(T &t, size_t /*id*/)
             {
-                auto idx = m_counter.fetch_add(1, atomics::memory_order_relaxed);
+                auto idx = m_pushes.fetch_add(1, atomics::memory_order_acquire);
                 assert(idx < m_size);
-                std::swap(t, m_bag[idx].value);
+                auto real_size = m_real_size.load(atomics::memory_order_acquire);
+                if (real_size == NO_SIZE || real_size <= idx) {
+                  return false;
+                }
+                auto& cell = m_bag[idx].value;
+                assert(!cell.flag.load(atomics::memory_order_acquire));
+                std::swap(t, cell.value);
+                cell.flag.store(true, atomics::memory_order_release);
                 return true;
             }
             bool extract(T &t)
             {
-                auto idx = m_counter.fetch_sub(1, atomics::memory_order_relaxed) - 1;
-                if (idx < 0) {
-                    // Yes, there is a race here.
-                    return false;
+                int real_size = NO_SIZE;
+                {
+                  auto pushes = m_pushes.load(atomics::memory_order_acquire);
+                  assert(pushes <= m_size);
+                  if(m_real_size.compare_exchange_strong(real_size, pushes, atomics::memory_order_acq_rel)) {
+                    real_size = pushes;
+                  }
                 }
-                std::swap(t, m_bag[idx].value);
+                auto idx = m_pops.fetch_add(1, atomics::memory_order_acquire);
+                assert(idx < m_size);
+                assert(real_size != NO_SIZE);
+                if(idx >= real_size) {
+                  return false;
+                }
+                auto& cell = m_bag[idx].value;
+                while(!cell.flag.load(atomics::memory_order_acquire));
+                std::swap(t, cell.value);
                 return true;
             }
 
+            /*
             bool empty() const { return m_counter.load(atomics::memory_order_acquire) <= 0; }
             size_t size() const
             {
                 auto size_ = m_counter.load(atomics::memory_order_acquire);
                 assert(size_ >= 0);
                 return size_;
-            }
+            }*/
         };
 
         template <class T>
@@ -76,6 +105,7 @@ namespace cds { namespace container {
             size_t m_size;
 
         public:
+            static constexpr const size_t c_nHazardPtrCount = 0; ///< Count of hazard pointer required for the algorithm
             IdBag(size_t ids) : m_size(ids)
             {
                 assert(m_size <= MAX_THREADS);
@@ -124,10 +154,12 @@ namespace cds { namespace container {
             struct inner_traits : treiber_stack::traits {
               using allocator = cds::details::memkind_allocator<T>;
             };
-            TreiberStack<cds::gc::HP, T, inner_traits> m_bag;
+            using bag_type = TreiberStack<cds::gc::HP, T, inner_traits>;
+            bag_type m_bag;
 
         public:
             StackBag(size_t /*ids*/) {}
+            static constexpr const size_t c_nHazardPtrCount = bag_type::c_nHazardPtrCount; ///< Count of hazard pointer required for the algorithm
 
             bool insert(T &t, size_t /*id*/)
             {
