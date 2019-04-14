49040482fdd49b4418e7c78368a8ba97202073ee
Or Ostrovsky <or.ostrovsky@gmail.com>
2019-04-14 18:32:51 +0300

    A working version

--- cds/intrusive/htm_basket_queue.h
+++ cds/intrusive/htm_basket_queue.h
@@ -20,25 +20,26 @@ namespace cds { namespace intrusive {
       struct htm_insert : basket_queue::atomics_insert<> {
         static constexpr bool IS_HTM = true;
         template <class MemoryModel, class MarkedPtr>
-        static InsertResult __attribute__((optimize("02"))) _(MarkedPtr old_node, MarkedPtr new_node, MarkedPtr& new_value, size_t thread_count = 1) {
-          new_node->m_pNext.store(MarkedPtr{}, std::memory_order_seq_cst);
+        static InsertResult _(MarkedPtr old_node, MarkedPtr new_node, MarkedPtr& new_value, size_t thread_count = 1) {
+          new_node->m_pNext.store(MarkedPtr{}, std::memory_order_relaxed);
           auto& old = old_node->m_pNext;
-          auto old_snapshot = old.load(std::memory_order_seq_cst);
-          // This check fixes everything.
-          //if (old_snapshot.ptr() != nullptr) {
-          //  new_value = old_snapshot;
-          //  return InsertResult::NOT_NULL;
-          //}
-          int ret;
+          int ret, ret2;
+          MarkedPtr pNext;
           while(true) {
             if ((ret = _xbegin()) == _XBEGIN_STARTED) {
-              MarkedPtr pNext = old.load(std::memory_order_seq_cst);
-              if (pNext.ptr() != nullptr) {
-                _xabort(0x01);
+              if(_xbegin() == _XBEGIN_STARTED) {
+                pNext = old.load(std::memory_order_relaxed);
+                if (pNext.ptr() != nullptr) {
+                  _xabort(0x01);
+                }
+                _xend();
               }
               delay(LATENCY * thread_count);
-              old.store(new_node, std::memory_order_release);
+              old.store(new_node, std::memory_order_relaxed);
               _xend();
+            }
+            if (ret == 0) {
+              continue;
             }
             if (ret == _XBEGIN_STARTED) {
               return InsertResult::SUCCESSFUL_INSERT;
@@ -46,27 +47,19 @@ namespace cds { namespace intrusive {
             if ((ret & _XABORT_EXPLICIT) != 0) {
               if(_XABORT_CODE(ret) != 0x01) {
                 std::stringstream s;
-                s << "Bad abort code " << _XABORT_CODE(ret) << ' ' << old_snapshot.all();
+                s << "Bad abort code " << _XABORT_CODE(ret);
                 throw std::logic_error(s.str());
               }
-              new_value = old.load(std::memory_order_seq_cst);
+              new_value = old.load(std::memory_order_acquire);
               return InsertResult::NOT_NULL;
             }
-            if (old_snapshot.ptr() != nullptr) {
-                std::stringstream s;
-                s << "Missed snapshot with " << std::hex << ret << ' ' << old_snapshot.ptr() << std::endl;
-                throw std::logic_error(s.str());
-            }
-            if ((ret & _XABORT_CONFLICT) != 0) {
+            bool is_conflict = (ret & _XABORT_CONFLICT) != 0;
+            bool is_nested = (ret & _XABORT_NESTED) != 0;
+            if (is_conflict && !is_nested) {
               delay(FINAL_LATENCY);
-              for(size_t i = 0; i < PATIENCE; ++ i) {
+              for(size_t p = 0; p < PATIENCE; ++ p) {
                 auto value = old.load(std::memory_order_acquire);
                 if(value.ptr() != nullptr) {
-                  if(value.ptr() == old_snapshot.ptr()) {
-                    std::stringstream s;
-                    s << "Bad update " << ret << ' ' << value.all() << ' ' << old_snapshot.all();
-                    throw std::logic_error(s.str());
-                  }
                   new_value = value;
                   return InsertResult::FAILED_INSERT;
                 }
